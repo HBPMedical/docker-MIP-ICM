4df562f96dca05b8f3e3b003f20f6d662920911a
Clementine Fourrier <cle.fourrier@gmail.com>
2017-11-16 17:08:50 +0100

    Idem

--- algo.py
+++ algo.py
@@ -2,20 +2,22 @@
 
 import logging
 import os
-#import sys
+import sys
+sys.path.append("/Users/clementine.fourrier/Documents/Projets/HBP/python-base-docker-images/python-mip/io_helper/io_helper/")
 import io_helper
 import utils
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    data = io_helper.fetch_data() #renverra des dataframes pandas => 2 novembre, envoyer message à Mirco
+    data = io_helper.fetch_data() #renverra des dataframes pandas => 2 novembre, envoyer message to Mirco
     print("This is the dataaaaa")
     print(data)
     print("End of the dataaaaa")
     #utils.write_input_to_file(data)
     os.system("./main.sh")
     pfa = utils.write_output_to_pfa("Multivariate")
+    print pfa
     error = 0
     shape = 0
     io_helper.save_results(pfa, error, shape)

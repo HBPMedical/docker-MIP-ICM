dfc1f3aa8062a9bdc2f164c4706dce8e6a5e3ce5
Clementine Fourrier <cle.fourrier@gmail.com>
2017-11-16 10:44:48 +0100

    Trying to get better visu on what is going on

--- algo.py
+++ algo.py
@@ -10,9 +10,9 @@ import utils
 def main():
     logging.basicConfig(level=logging.INFO)
     data = io_helper.fetch_data() #renverra des dataframes pandas => 2 novembre, envoyer message à Mirco
-    print "This is the dataaaaa"
+    print("This is the dataaaaa")
     print(data)
-    print "End of the dataaaaa"
+    print("End of the dataaaaa")
     #utils.write_input_to_file(data)
     os.system("./main.sh")
     pfa = utils.write_output_to_pfa("Multivariate")

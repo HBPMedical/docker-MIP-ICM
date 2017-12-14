d52b2b41395087510d758b50a98db5702917f8ab
Clementine Fourrier <cle.fourrier@gmail.com>
2017-12-14 15:52:03 +0100

    Now using master branch

--- python-simplebuild/main.py
+++ python-simplebuild/main.py
@@ -13,16 +13,13 @@ from utils import utils
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    #try:
-    #    data = io_helper.fetch_data() #renverra des dataframes pandas => 2 novembre, envoyer message to Mirco
-    #    model_type = utils.write_input_to_file(data)
-    #except:
-    #    model_type = "univariate"
+    try:
+        data = io_helper.fetch_data() #renverra des dataframes pandas => 2 novembre, envoyer message to Mirco
+        model_type = utils.write_input_to_file(data)
+    except:
+        model_type = "univariate"
 
-    model_type = "multivariate"
-    print(model_type)
     os.system("./main_" + model_type + ".sh")
-    print("end of run")
     highchart = utils.write_output_to_highchart(model_type)
     print(highchart)
     io_helper.save_results(highchart, "", "highcharts_json")

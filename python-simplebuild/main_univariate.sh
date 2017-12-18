e292ac56fc0ab7bac16240662462f47a322d8cbe
Clementine Fourrier <cle.fourrier@gmail.com>
2017-12-18 16:28:56 +0100

    Test

--- python-simplebuild/main_univariate.sh
+++ python-simplebuild/main_univariate.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd longitudina/examples/scalar_models/univariate/sigmoid/
-exec ../../../build/Longitudina fit \
+exec ../../../../build/Longitudina fit \
 settings/model_settings.xml \
 settings/algorithm_settings.xml \
 settings/data_settings.xml \

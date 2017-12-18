ccc60139b6a3efa9d9f23fc793d412f7b8156d88
Clementine Fourrier <cle.fourrier@gmail.com>
2017-12-18 15:46:09 +0100

    Test

--- python-simplebuild/main.py
+++ python-simplebuild/main.py
@@ -13,11 +13,13 @@ from utils import utils
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    try:
-        data = io_helper.fetch_data() #renverra des dataframes pandas => 2 novembre, envoyer message to Mirco
-        model_type = utils.write_input_to_file(data)
-    except:
-        model_type = "univariate"
+#    try:
+#        data = io_helper.fetch_data() #renverra des dataframes pandas => 2 novembre, envoyer message to Mirco
+#        model_type = utils.write_input_to_file(data)
+#    except:
+#        model_type = "univariate"
+
+    model_type = "univariate"
 
     os.system("./main_" + model_type + ".sh")
     highchart = utils.write_output_to_highchart(model_type)
